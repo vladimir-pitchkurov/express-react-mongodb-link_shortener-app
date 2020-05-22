ecaf5c8a693b5449d5971b084dc6e9c00755f873
vladimir-pit <vladimirpitbul@gmail.com>
2020-05-22 21:26:54 +0300

    prepare for connection to mongo db v0.1.0

--- app.js
+++ app.js
@@ -7,17 +7,16 @@ const PORT = config.get('port') || 5000;
 
 async function start() {
     try {
-        await mongoose.connect(config.get('mongoUri'), {
-
-        });
+        await mongoose.connect(config.get('mongoUri'), {});
+        app.listen(PORT, () => {
+            console.log(`App started on port ${PORT}`)
+        })
     } catch (e) {
         console.log('server ERROR: ', e.message);
+        process.exit(1);
     }
 }
 
 start();
 
 
-app.listen(PORT, () => {
-    console.log(`App started on port ${PORT}`)
-})
