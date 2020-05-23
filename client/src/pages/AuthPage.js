180d3a9282433d2c22fd5922a0d5a4ed2c0cfe8d
vladimir-pit <vladimirpitbul@gmail.com>
2020-05-23 15:19:04 +0300

    create auth request

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import {useHttp} from "../../hooks/http.hook";
 
 export const AuthPage = () => {
+    const {loading, request, error} = useHttp();
+
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    });
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', ...form);
+            console.log('data', data);
+        } catch (e) {
+
+        }
+    }
+
     return (
         <div className="row">
             <div className="col s6 offset-s3">
@@ -8,16 +29,17 @@ export const AuthPage = () => {
                     <div className="card-content white-text">
                         <span className="card-title">Authorization</span>
                         <div>
-                           <div className="input-field">
-                               <input
-                                   type="text"
-                                   placeholder="enter your email"
-                                   id="email"
-                                   name="email"
-                                   className="yellow-input"
-                               />
-                               <label htmlFor="email">Email</label>
-                           </div>
+                            <div className="input-field">
+                                <input
+                                    type="text"
+                                    placeholder="enter your email"
+                                    id="email"
+                                    name="email"
+                                    className="yellow-input"
+                                    onChange={changeHandler}
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
                             <div className="input-field">
                                 <input
                                     type="password"
@@ -25,14 +47,26 @@ export const AuthPage = () => {
                                     id="password"
                                     name="password"
                                     className="yellow-input"
+                                    onChange={changeHandler}
                                 />
                                 <label htmlFor="password">Password</label>
                             </div>
                         </div>
                     </div>
                     <div className="card-action login_buttons">
-                        <button className="btn yellow darken-4">Login</button>
-                        <button className="btn grey lighten-1 black-text">Sign Up</button>
+                        <button
+                            className="btn yellow darken-4"
+                            disabled={loading}
+                        >
+                            Login
+                        </button>
+                        <button
+                            className="btn grey lighten-1 black-text"
+                            onClick={registerHandler}
+                            disabled={loading}
+                        >
+                            Sign Up
+                        </button>
                     </div>
                 </div>
             </div>
