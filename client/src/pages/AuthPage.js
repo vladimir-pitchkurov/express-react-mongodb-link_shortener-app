9101139cdb989438dac877df02b64fabf7d96a29
vladimir-pit <vladimirpitbul@gmail.com>
2020-05-23 14:49:36 +0300

    create auth form

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,8 +2,40 @@ import React from "react";
 
 export const AuthPage = () => {
     return (
-        <div>
-            <h1>AuthPage</h1>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <div className="card blue darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Authorization</span>
+                        <div>
+                           <div className="input-field">
+                               <input
+                                   type="text"
+                                   placeholder="enter your email"
+                                   id="email"
+                                   name="email"
+                                   className="yellow-input"
+                               />
+                               <label htmlFor="email">Email</label>
+                           </div>
+                            <div className="input-field">
+                                <input
+                                    type="password"
+                                    placeholder="enter your password"
+                                    id="password"
+                                    name="password"
+                                    className="yellow-input"
+                                />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-action login_buttons">
+                        <button className="btn yellow darken-4">Login</button>
+                        <button className="btn grey lighten-1 black-text">Sign Up</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
