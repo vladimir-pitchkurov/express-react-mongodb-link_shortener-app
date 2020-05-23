07d755dd4db07206c197bcf6eda513aae508308f
vladimir-pit <vladimirpitbul@gmail.com>
2020-05-23 22:37:24 +0300

    finalize CreatePage.js

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,16 +1,18 @@
 import React, {useContext, useEffect, useState} from "react";
 import {useHttp} from "../hooks/http.hook";
 import {AuthContext} from "../context/AuthContext";
+import {useHistory} from 'react-router-dom'
 
 export const CreatePage = () => {
     const [link, setLink] = useState('')
     const {request} = useHttp()
+    const history = useHistory()
     const auth = useContext(AuthContext)
     const pressHandler = async event => {
         if (event.key === 'Enter') {
             try {
                 const data = await request('/api/link/generate', 'POST', {from: link}, {Authorization: `Bearer ${auth.token}`})
-                console.log(data);
+                history.push(`/detail/${data.link._id}`)
             } catch (e) {
 
             }
