570ad7d0e436fc6d312a14b968f96ad6c140958f
vladimir-pit <vladimirpitbul@gmail.com>
2020-05-23 22:59:55 +0300

    show loader and correct current page

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {useRoutes} from "./routes";
 import {useAuth} from "./hooks/auth.hook";
-import 'materialize-css';
-import {AuthContext} from "./context/AuthContext";
 import {Navbar} from "./components/Navbar";
+import {AuthContext} from "./context/AuthContext";
+import {Loader} from "./components/Loader";
+import 'materialize-css';
 
 function App() {
-    const {token, userId, login, logout} = useAuth()
+    const {token, userId, login, logout, ready} = useAuth()
     const isAuthenticated = !!token
     const routes = useRoutes(isAuthenticated)
+    if (!ready) {
+        return <Loader/>
+    }
+
     return (
         <AuthContext.Provider value={{
             token, login, logout, userId, isAuthenticated
