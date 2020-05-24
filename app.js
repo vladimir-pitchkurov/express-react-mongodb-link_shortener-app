505913548e58691e997d8bbec3fae6ca1fe8edc2
vladimir-pit <vladimirpitbul@gmail.com>
2020-05-24 20:29:53 +0300

    prepare for deploy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const path = require('path');
 
 app = express();
 const PORT = config.get('port') || 5000;
@@ -10,6 +11,14 @@ app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
+
+if (process.enc.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, resp) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 async function start() {
     try {
