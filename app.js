28f1d5fe1b93634ec1f2d4027565b0fb1464ba46
vladimir-pit <vladimirpitbul@gmail.com>
2020-05-27 08:49:28 +0300

    bug-fix

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
     app.get('*', (req, resp) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        resp.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
